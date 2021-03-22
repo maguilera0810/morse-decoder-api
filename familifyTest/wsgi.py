d3bd0fe71133104254cf9e957bcd8544ad6fb8e9
Mauricio Aguilera <maguilera0810@gmail.com>
2021-03-22 03:16:21 -0500

    Primer Commit

--- familifyTest/wsgi.py
+++ familifyTest/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -16,3 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'familifyTest.settings')
 
 application = get_wsgi_application()
 #application = WhiteNoise(application)
+
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+print(os.getcwd())
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'familifyTest.settings')
