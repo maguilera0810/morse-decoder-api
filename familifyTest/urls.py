21ced3cd5cf65c914659a698a9bc8fbcd22ee67a
Mauricio Aguilera <maguilera0810@gmail.com>
2021-03-22 01:03:43 -0500

    Procfile

--- familifyTest/urls.py
+++ familifyTest/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.config import settings
+from django.config.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('', include('applications.morse.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
