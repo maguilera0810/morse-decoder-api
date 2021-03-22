220290236fe3156ab9c007b6f43e99a5080dc52a
Mauricio Aguilera <maguilera0810@gmail.com>
2021-03-22 02:58:16 -0500

    Primer Commit

--- familifyTest/settings.py
+++ familifyTest/settings.py
@@ -85,6 +85,7 @@ WSGI_APPLICATION = 'familifyTest.wsgi.application'
 #        default=config('DATABASE_URL')
 #    )
 #}
+DATABASES = {}
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
