084d8582eda51c8d0ec5d722f09c2aaaad209067
Mauricio Aguilera <maguilera0810@gmail.com>
2021-03-22 03:03:07 -0500

    Primer Commit

--- familifyTest/settings.py
+++ familifyTest/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static')
+#STATICFILES_DIRS = (BASE_DIR / 'static')
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
