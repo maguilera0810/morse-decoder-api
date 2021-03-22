aba20e15a049fedc22f7c185a4f0e51537cc5e13
Mauricio Aguilera <maguilera0810@gmail.com>
2021-03-22 03:00:46 -0500

    Primer Commit

--- familifyTest/settings.py
+++ familifyTest/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'familifyTest.urls'
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # JWT
 
